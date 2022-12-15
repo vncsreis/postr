f6c00c12e798e2e66e3ce4b88dec2b89dd1b8e3c
me <me@example.com>
2022-12-15 14:54:07 -0300

    added userInfo and post to modal context

--- context/ui/ModalContext/ModalContext.tsx
+++ context/ui/ModalContext/ModalContext.tsx
@@ -1,15 +1,34 @@
 import React, { PropsWithChildren, useContext, useState } from "react";
+import Post from "../../../models/post";
+import { UserInfo } from "../../../models/user";
 
-export const ModalContext = React.createContext({
+interface ModalContextProps {
+  isOpen: boolean;
+  setOpen: (x: boolean) => void;
+  userInfo: UserInfo | null;
+  setUserInfo: (userInfo: UserInfo) => void;
+  post: Post | null;
+  setPost: (post: Post) => void;
+}
+
+export const ModalContext = React.createContext<ModalContextProps>({
   isOpen: false,
   setOpen: (x: boolean) => {},
+  userInfo: null,
+  setUserInfo: (u: UserInfo) => {},
+  post: null,
+  setPost: (p: Post) => {},
 });
 
 export const ModalProvider = ({ children }: PropsWithChildren) => {
   const [isOpen, setOpen] = useState(false);
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const [post, setPost] = useState<Post | null>(null);
 
   return (
-    <ModalContext.Provider value={{ isOpen, setOpen }}>
+    <ModalContext.Provider
+      value={{ isOpen, setOpen, userInfo, setUserInfo, post, setPost }}
+    >
       {children}
     </ModalContext.Provider>
   );
