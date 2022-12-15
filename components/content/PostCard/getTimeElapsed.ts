2f22981ef228ebebee2938a0c2eb36f3f541c3b0
me <me@example.com>
2022-12-15 14:33:59 -0300

    fix function logic

--- components/content/PostCard/getTimeElapsed.ts
+++ components/content/PostCard/getTimeElapsed.ts
@@ -1,24 +1,31 @@
 export default function getTimeElapsed(then: Date, now: Date) {
   const ellapsedMilis = now.getTime() - then.getTime();
+  console.log(ellapsedMilis);
 
   return getUnit(ellapsedMilis);
 }
 
+// TODO: Test this function
 function getUnit(time: number) {
-  const SECOND = 10e3;
-  const MINUTE = 1.67 * 10e5;
-  const HOUR = 2.78 * 10e7;
-  const DAY = 1.16 * 10e8;
+  const SECOND = 1000;
+  const MINUTE = 60000;
+  const HOUR = 3.6e6;
+  const DAY = 8.64e7;
 
   if (time < 60 * SECOND) {
-    return `${Math.round(time / SECOND)}s`;
+    return `${Math.floor(time / SECOND)}s`;
   } else if (time < 60 * MINUTE) {
-    return `${Math.round(time / MINUTE)}m`;
+    return `${Math.floor(time / MINUTE)}m`;
   } else if (time < 24 * HOUR) {
-    return `${Math.round(time / HOUR)}h`;
+    return `${Math.floor(time / HOUR)}h`;
   } else if (time < 7 * DAY) {
-    return `${Math.round(time / DAY)}d`;
+    return `${Math.floor(time / DAY)}d`;
   } else {
-    return new Date(Date.parse(time.toString())).toLocaleDateString();
+    return new Date(Date.parse(time.toString()))
+      .toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+      })
+      .toLowerCase();
   }
 }
