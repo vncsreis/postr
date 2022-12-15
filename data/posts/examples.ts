1e388de2403577eb1258377f6127005a31cfeaac
me <me@example.com>
2022-12-15 14:01:12 -0300

    created example data folder (TODO: replace with backend connection)

---
+++ data/posts/examples.ts
@@ -0,0 +1,13 @@
+import Post from "../../models/post";
+
+export const post01: Post = {
+  answers: null,
+  date: new Date(Date.parse("Thu Dec 15 2022 13:52:11 GMT-0300")),
+  edited: false,
+  id: "1",
+  lastVersion: null,
+  likes: 0,
+  mentions: null,
+  text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  userId: "1",
+};
---
+++ data/users/example.ts
@@ -0,0 +1,7 @@
+import { UserInfo } from "../../models/user";
+
+export const user01: UserInfo = {
+  id: "1",
+  name: "Bob Test",
+  username: "bobtest",
+};
